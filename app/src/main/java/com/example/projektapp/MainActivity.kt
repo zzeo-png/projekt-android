532aedeafbc690cef4a80bfe405d0c0889b2948b
zzeo-png <red.builder.h2o@gmail.com>
2023-06-02 20:36:26 +0200

    updated tracking
    - removed redundant functions
    - simplified code

--- app/src/main/java/com/example/projektapp/MainActivity.kt
+++ app/src/main/java/com/example/projektapp/MainActivity.kt
@@ -29,18 +29,9 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.example.projektapp.databinding.ActivityMainBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.Granularity
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
+import com.google.android.gms.location.*
 import com.google.gson.Gson
-import kotlinx.coroutines.CompletableDeferred
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import org.json.JSONObject
 import java.nio.charset.Charset
 import kotlin.coroutines.resume
@@ -50,7 +41,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var text1: TextView
-    private lateinit var testButton: Button
+    private lateinit var startTracking: Button
+    private lateinit var stopTracking: Button
     private lateinit var startButton: Button
     private lateinit var stopButton: Button
     private lateinit var sensorManager: SensorManager
@@ -63,7 +55,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var locationRequest: LocationRequest
     private var startPosition: Location = Location("")
     private var endPosition: Location = Location("")
-    private val endPositionDeferred = CompletableDeferred<Location>()
+    private var startPositionDeferred = CompletableDeferred<Location>()
+    private var endPositionDeferred = CompletableDeferred<Location>()
+    private var trackingStatus: Boolean = false
+    private var analyzingCoroutine: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,7 +71,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // nastavi accelometer
         accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
-        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 1000).apply {
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 2000).apply {
             setMinUpdateDistanceMeters(distanceInterval)
             setGranularity(Granularity.GRANULARITY_FINE)
             setWaitForAccurateLocation(true)
@@ -86,41 +81,72 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             override fun onLocationResult(locationResult: LocationResult) {
                 locationResult ?: return
                 for(location in locationResult.locations){
-                    endPositionDeferred.complete(location)
+                    Log.i("LEO123", "location update")
+                    if(startPositionDeferred.isCompleted){
+                        endPositionDeferred.complete(location)
+                    }
+                    else{
+                        startPositionDeferred.complete(location)
+                    }
                 }
             }
         }
 
-        testButton = binding.testButton
+        startTracking = binding.startTracking
+        stopTracking = binding.stopTracking
         text1 = binding.text1
 
-        testButton.setOnClickListener{
+        startTracking.setOnClickListener{
             startAnalyzing()
+        }
+        stopTracking.setOnClickListener{
+            trackingStatus = false
+            stopLocationUpdates()
+            stopReading(findViewById(R.id.btn_startA))
+            analyzingCoroutine?.cancel()
         }
     }
 
     private fun startAnalyzing(){
-        CoroutineScope(Dispatchers.Main).launch {
-            // čakaj na začetno lokacijo
-            startPosition = getCurrentLocationAsync()
+        trackingStatus = true
+        analyzingCoroutine = CoroutineScope(Dispatchers.Main).launch {
+            while(trackingStatus){
+                // čakaj na začetno lokacijo
+                Log.i("LEO123", "started...")
+                //startPosition = getCurrentLocationAsync()
 
-            // začni sledenje lokacije
-            startLocationUpdates()
+                // začni sledenje lokacije
+                startLocationUpdates()
 
-            // čakaj na končno lokacijo
-            endPosition = endPositionDeferred.await()
+                if(!startPositionDeferred.isCompleted){
+                    startPosition = startPositionDeferred.await()
+                }
+                resumeReading(findViewById(R.id.btn_startA))
+                Log.i("LEO123", "got start position")
 
-            val data = RoadData(
-                startPosition.latitude,
-                startPosition.longitude,
-                endPosition.latitude,
-                endPosition.longitude
-            )
+                // čakaj na končno lokacijo
+                endPosition = endPositionDeferred.await()
+                Log.i("LEO123", "got end position")
 
-            stopLocationUpdates()
+                // kalkuliraj podatke za stanje
 
-            // pošlji rezultate
-            postToWeb(data)
+                val data = RoadData(
+                    startPosition.latitude,
+                    startPosition.longitude,
+                    endPosition.latitude,
+                    endPosition.longitude,
+                    0
+                )
+
+                // ponastavi točki
+                startPosition = endPosition
+                //startPositionDeferred = CompletableDeferred<Location>()
+                endPositionDeferred = CompletableDeferred<Location>()
+
+                // pošlji rezultate
+                Log.i("LEO123", "Sending data...")
+                postToWeb(data)
+            }
         }
     }
 
@@ -128,7 +154,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         // Volley
         val volleyQueue = Volley.newRequestQueue(this)
         //val url = "http://34.65.105.245:3001/roads"
-        val url = "http://192.168.1.6:3001/roads"
+        val url = "http://192.168.1.100:3001/roads"
 
         val postRequest = object: StringRequest(
             Method.POST, url,
@@ -171,28 +197,6 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
-    private fun getCurrentLocation(callback: (Location) -> Unit) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    if(location != null){
-                        callback(location)
-                    }
-                    else{
-                        callback(Location(""))
-                    }
-                }
-        } else {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1001)
-        }
-    }
-
-    private suspend fun getCurrentLocationAsync(): Location = suspendCoroutine { continuation ->
-        getCurrentLocation { location ->
-            continuation.resume(location)
-        }
-    }
-
     override fun onSensorChanged(event: SensorEvent?) {
         if(event != null && resume){
             if(event.sensor.type == Sensor.TYPE_ACCELEROMETER){
@@ -215,11 +219,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)
     }
 
-    fun resumeReading(view: View){
+    private fun resumeReading(view: View){
         this.resume = true
     }
 
-    fun stopReading(view: View){
+    private fun stopReading(view: View){
         this.resume = false
     }
 
